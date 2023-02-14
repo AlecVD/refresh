251e4af6bcf0d9fcdab8fff1c210e92f7ee1c528
alecvd <14879968+AlecVD@users.noreply.github.com>
2023-02-13 17:51:46 -0800

    Add player movement

--- core/src/com/alecvd/refresh/Refresh.java
+++ core/src/com/alecvd/refresh/Refresh.java
@@ -18,19 +18,40 @@ import com.badlogic.gdx.utils.TimeUtils;
 public class Refresh extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture playerImage;
+	private Texture enemyImage;
+	private OrthographicCamera camera;
+	private Rectangle player;
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
+
 		playerImage = new Texture(Gdx.files.internal("Player/p1_front.png"));
+		enemyImage = new Texture(Gdx.files.internal("Enemies/blockerMad.png"));
+
+		player = new Rectangle();
+		player.x = 100;
+		player.y = 100;
+		player.width = 66;
+		player.height = 92;
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+
 		batch.begin();
-		batch.draw(playerImage, 0, 0);
+		batch.draw(playerImage, player.x, player.y);
 		batch.end();
+
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) player.x -= 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) player.x += 200 * Gdx.graphics.getDeltaTime();
 	}
 	
 	@Override
